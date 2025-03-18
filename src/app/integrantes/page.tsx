46a6c2f3fa46c05309075f84ac8e801d66be53f3
castropatrick <patrickcastro73@gmail.com>
2025-03-18 19:52:46 -0300

    Adicionando React Fragment nas rotas.

--- src/app/integrantes/page.tsx
+++ src/app/integrantes/page.tsx
@@ -6,89 +6,91 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Integrante = () => {
     return (
-        <main className="mt-[10vh]">
-            <section className="text-center">
-                <h1 className="text-[6rem] font-extrabold text-white">Conheça o nosso time</h1>
-                <p className="mt-[2vh] text-4xl font-extrabold opacity-70 text-white">1TDSPB</p>
-            </section>
+        <>
+            <main className="mt-[10vh]">
+                <section className="text-center">
+                    <h1 className="text-[6rem] font-extrabold text-white">Conheça o nosso time</h1>
+                    <p className="mt-[2vh] text-4xl font-extrabold opacity-70 text-white">1TDSPB</p>
+                </section>
 
-            <div className="flex justify-center gap-[5vw] p-[4em] flex-wrap max-w-full mx-auto mt-[3vh]">
-                <figure className="bg-[#f0f0f03a] p-10 rounded-xl text-center transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                    <Image
-                        src="/assets/imagem-gabriel.jpg"
-                        alt="Foto do integrante 1"
-                        width={200}
-                        height={200}
-                        className="w-48 h-48 rounded-full object-cover transition-transform duration-300"
-                    />
-                    <figcaption className="w-full mt-8 text-lg font-medium text-white">
-                        <p>Gabriel Oliveira Rossi</p>
-                        <p className="opacity-70 mt-2">Rm 560967</p>
-                        <div className="flex mt-[3vh] justify-center gap-3">
-                            <a href="https://github.com/GabrielRossi01" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                                <FontAwesomeIcon icon={faGithub} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/gabriel-oliveira-rossi-155baa324/" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                                <FontAwesomeIcon icon={faLinkedin} />
-                            </a>
-                            <a href="mailto:rm560967@fiap.com.br" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                                <FontAwesomeIcon icon={faEnvelope} />
-                            </a>
-                        </div>
-                    </figcaption>
-                </figure>
+                <div className="flex justify-center gap-[5vw] p-[4em] flex-wrap max-w-full mx-auto mt-[3vh]">
+                    <figure className="bg-[#f0f0f03a] p-10 rounded-xl text-center transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                        <Image
+                            src="/assets/imagem-gabriel.jpg"
+                            alt="Foto do integrante 1"
+                            width={200}
+                            height={200}
+                            className="w-48 h-48 rounded-full object-cover transition-transform duration-300"
+                        />
+                        <figcaption className="w-full mt-8 text-lg font-medium text-white">
+                            <p>Gabriel Oliveira Rossi</p>
+                            <p className="opacity-70 mt-2">Rm 560967</p>
+                            <div className="flex mt-[3vh] justify-center gap-3">
+                                <a href="https://github.com/GabrielRossi01" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                                    <FontAwesomeIcon icon={faGithub} />
+                                </a>
+                                <a href="https://www.linkedin.com/in/gabriel-oliveira-rossi-155baa324/" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                                    <FontAwesomeIcon icon={faLinkedin} />
+                                </a>
+                                <a href="mailto:rm560967@fiap.com.br" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                                    <FontAwesomeIcon icon={faEnvelope} />
+                                </a>
+                            </div>
+                        </figcaption>
+                    </figure>
 
-                <figure className="bg-[#f0f0f03a] p-10 rounded-xl text-center transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                    <Image
-                        src="/assets/imagem-patrick.jpg"
-                        alt="Foto do integrante 2"
-                        width={200}
-                        height={200}
-                        className="w-48 h-48 rounded-full object-cover transition-transform duration-300"
-                    />
-                    <figcaption className="w-full mt-8 text-lg font-medium text-white">
-                        <p>Patrick Castro Quintana</p>
-                        <p className="opacity-70 mt-2">Rm 559271</p>
-                        <div className="flex mt-[3vh] justify-center gap-3">
-                            <a href="https://github.com/castropatrick" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                                <FontAwesomeIcon icon={faGithub} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/patrick-castro-839aa2273/" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                                <FontAwesomeIcon icon={faLinkedin} />
-                            </a>
-                            <a href="mailto:rm559271@fiap.com.br" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                                <FontAwesomeIcon icon={faEnvelope} />
-                            </a>
-                        </div>
-                    </figcaption>
-                </figure>
+                    <figure className="bg-[#f0f0f03a] p-10 rounded-xl text-center transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                        <Image
+                            src="/assets/imagem-patrick.jpg"
+                            alt="Foto do integrante 2"
+                            width={200}
+                            height={200}
+                            className="w-48 h-48 rounded-full object-cover transition-transform duration-300"
+                        />
+                        <figcaption className="w-full mt-8 text-lg font-medium text-white">
+                            <p>Patrick Castro Quintana</p>
+                            <p className="opacity-70 mt-2">Rm 559271</p>
+                            <div className="flex mt-[3vh] justify-center gap-3">
+                                <a href="https://github.com/castropatrick" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                                    <FontAwesomeIcon icon={faGithub} />
+                                </a>
+                                <a href="https://www.linkedin.com/in/patrick-castro-839aa2273/" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                                    <FontAwesomeIcon icon={faLinkedin} />
+                                </a>
+                                <a href="mailto:rm559271@fiap.com.br" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                                    <FontAwesomeIcon icon={faEnvelope} />
+                                </a>
+                            </div>
+                        </figcaption>
+                    </figure>
 
-                <figure className="bg-[#f0f0f03a] p-10 rounded-xl text-center transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                    <Image
-                        src="/assets/imagem-rodrigo.jpg"
-                        alt="Foto do integrante 3"
-                        width={200}
-                        height={200}
-                        className="w-48 h-48 rounded-full object-cover transition-transform duration-300"
-                    />
-                    <figcaption className="w-full mt-8 text-lg font-medium text-white">
-                        <p>Rodrigo Naoki Yamasaki</p>
-                        <p className="opacity-70 mt-2">Rm 560759</p>
-                        <div className="flex mt-[3vh] justify-center gap-3">
-                            <a href="https://github.com/RodrygoYamasaki" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                                <FontAwesomeIcon icon={faGithub} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/rodrigo-yamasaki-74a3b1324/" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                                <FontAwesomeIcon icon={faLinkedin} />
-                            </a>
-                            <a href="mailto:rm560759@fiap.com.br" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
-                                <FontAwesomeIcon icon={faEnvelope} />
-                            </a>
-                        </div>
-                    </figcaption>
-                </figure>
-            </div>
-        </main>
+                    <figure className="bg-[#f0f0f03a] p-10 rounded-xl text-center transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                        <Image
+                            src="/assets/imagem-rodrigo.jpg"
+                            alt="Foto do integrante 3"
+                            width={200}
+                            height={200}
+                            className="w-48 h-48 rounded-full object-cover transition-transform duration-300"
+                        />
+                        <figcaption className="w-full mt-8 text-lg font-medium text-white">
+                            <p>Rodrigo Naoki Yamasaki</p>
+                            <p className="opacity-70 mt-2">Rm 560759</p>
+                            <div className="flex mt-[3vh] justify-center gap-3">
+                                <a href="https://github.com/RodrygoYamasaki" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                                    <FontAwesomeIcon icon={faGithub} />
+                                </a>
+                                <a href="https://www.linkedin.com/in/rodrigo-yamasaki-74a3b1324/" target="_blank" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                                    <FontAwesomeIcon icon={faLinkedin} />
+                                </a>
+                                <a href="mailto:rm560759@fiap.com.br" className="text-xl transition-all duration-300 hover:translate-y-[-5px] hover:shadow-lg">
+                                    <FontAwesomeIcon icon={faEnvelope} />
+                                </a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+            </main>
+        </>
     );
 };
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,57 +3,59 @@ import Link from 'next/link';
 
 const Login = () => {
   return (
-    <main className="mt-[10vh]">
-      <section>
-        <h1 className="ml-[13.5vw] w-[60%] text-[6rem] font-extrabold text-white">
-          Bem-vindo, faça login para continuar
-        </h1>
+    <>
+      <main className="mt-[10vh]">
+        <section>
+          <h1 className="ml-[13.5vw] w-[60%] text-[6rem] font-extrabold text-white">
+            Bem-vindo, faça login para continuar
+          </h1>
 
-        <form aria-live="polite" id="formLogin" className="p-4">
-          <div className="mb-4">
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email:"
-              required
-              className="bg-white ml-[12.5vw] w-[40%] p-6 mt-4 rounded-xl border-none font-primary"
-            />
-          </div>
+          <form aria-live="polite" id="formLogin" className="p-4">
+            <div className="mb-4">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email:"
+                required
+                className="bg-white ml-[12.5vw] w-[40%] p-6 mt-4 rounded-xl border-none font-primary"
+              />
+            </div>
 
-          <div className="mb-4">
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Senha:"
-              required
-              className="bg-white ml-[12.5vw] w-[40%] p-6 mt-4 rounded-xl border-none font-primary"
-            />
-          </div>
+            <div className="mb-4">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Senha:"
+                required
+                className="bg-white ml-[12.5vw] w-[40%] p-6 mt-4 rounded-xl border-none font-primary"
+              />
+            </div>
 
-          <div className="mt-[5vh] ml-[27vw]">
-            <Link href="/home">
-              <button
-                type="submit"
-                className="bg-[var(--cor-botao)] w-[15%] text-center border-none p-4 text-xl cursor-pointer rounded-[16px] transition-all ease-in-out duration-300 block text-[var(--cor-primaria)] font-semibold hover:bg-[var(--botao-hover)] hover:scale-105"
-              >
-                Login
-              </button>
-            </Link>
+            <div className="mt-[5vh] ml-[27vw]">
+              <Link href="/home">
+                <button
+                  type="submit"
+                  className="bg-[var(--cor-botao)] w-[15%] text-center border-none p-4 text-xl cursor-pointer rounded-[16px] transition-all ease-in-out duration-300 block text-[var(--cor-primaria)] font-semibold hover:bg-[var(--botao-hover)] hover:scale-105"
+                >
+                  Login
+                </button>
+              </Link>
 
-            <p className="text-white mt-4 -ml-9">
-              Ainda não é registrado?
-              <Link href="/registro" className="no-underline">
-                <span className="text-[var(--cor-secundaria)] font-semibold hover:underline ml-1">
-                  Registre-se
-                </span>
-              </Link> 
-            </p>
-          </div>
-        </form>
-      </section>
-    </main>
+              <p className="text-white mt-4 -ml-9">
+                Ainda não é registrado?
+                <Link href="/registro" className="no-underline">
+                  <span className="text-[var(--cor-secundaria)] font-semibold hover:underline ml-1">
+                    Registre-se
+                  </span>
+                </Link>
+              </p>
+            </div>
+          </form>
+        </section>
+      </main>
+    </>
   )
 };
 
