18e21a6429a8120ba8b587cd723a517f3a53cd36
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-03-20 08:29:13 -0300

    Aplicando estilos responsivos com a propriedade sm na rota home.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,18 @@
-import Head from 'next/head';
 import '../globals.css';
 import Image from 'next/image';
-
+ 
 const Home = () => {
     return (
         <>
-            <Head>
-                <title>Home</title>
-                <meta name="description" content="Agora ficou fácil andar nos trilhos" />
-            </Head>
             <main className="mt-[10vh] flex flex-col items-center">
                 <section>
-                    <h1 className="ml-[13.5vw] w-[60%] text-[6rem] font-extrabold text-white">
+                    <h1 className="ml-[13.5vw] w-[60%] text-[6rem] font-extrabold text-white
+                        sm:w-[60%] sm:text-[6rem]">
                         Agora ficou fácil andar nos trilhos
                     </h1>
-
-                    <ul className="flex flex-col list-none mt-[10vh] gap-[10rem]">
-                        <li className="flex flex-col items-center p-6 rounded-lg">
+ 
+                    <ul className="flex flex-col list-none mt-[10vh] gap-[10rem] sm:gap-[5rem]">
+                        <li className="flex flex-col items-center p-6 rounded-lg sm:p-4 sm:w-[100%]">
                             <figure className="flex flex-col items-center gap-2">
                                 <Image
                                     src="/assets/grupo-verdee.png"
@@ -29,11 +25,11 @@ const Home = () => {
                                     Monitoramento
                                 </figcaption>
                             </figure>
-                            <p className="text-lg font-medium opacity-70 mt-2 text-center text-white">
+                            <p className="text-lg font-medium opacity-70 mt-2 text-center text-white sm:w-[90%]">
                                 Análise contínua do estado das estações e fluxo de passageiros.
                             </p>
                         </li>
-
+ 
                         <li className="flex flex-col items-center p-6 rounded-lg">
                             <figure className="flex flex-col items-center gap-2">
                                 <Image
@@ -41,13 +37,13 @@ const Home = () => {
                                     alt="Ícone de mapa"
                                     width={150}
                                     height={150}
-                                    className="w-4/5 h-4/5 transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg"
+                                    className="w-4/5 h-4/5 transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg sm:w-[70%] sm:h-auto"
                                 />
                                 <figcaption className="text-xl mt-2 font-bold text-white">
                                     Mapa
                                 </figcaption>
                             </figure>
-                            <p className="text-lg font-medium opacity-70 mt-2 text-center text-white">
+                            <p className="text-lg font-medium opacity-70 mt-2 text-center text-white sm:w-[90%]">
                                 Visualize as linhas de trens e metrôs do estado de SP.
                             </p>
                         </li>
@@ -55,8 +51,8 @@ const Home = () => {
                 </section>
             </main>
         </>
-
+ 
     )
 };
-
+ 
 export default Home;
